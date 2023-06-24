b328c7d068c5d1ba08b9412933ba8aa98f2e0bdd
IgorBels <118687894+IgorBels@users.noreply.github.com>
2023-06-24 09:46:46 +0300

    Add files via upload

---
+++ acme_project/birthday/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from .forms import BirthdayForm
+
+from .utils import calculate_birthday_countdown
+
+
+def birthday(request):
+    template = 'birthday/birthday.html'
+    form = BirthdayForm(request.POST or None)
+    context = {'form': form}
+    if form.is_valid():
+        form.save()
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday']
+        )
+        context.update({'birthday_countdown': birthday_countdown})
+    
+    return render(request, template, context)
